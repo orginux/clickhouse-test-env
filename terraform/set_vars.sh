9af5734b854c747f6891f922c32ae14ef94c9a6d
orginux <orginux@protonmail.com>
2022-02-13 21:45:35 +0300

    read keys from file

--- terraform/set_vars.sh
+++ terraform/set_vars.sh
@@ -1,9 +1,19 @@
-echo -n "AWS_ACCESS_KEY_ID:"
-read KEY_ID
-echo -n "AWS_SECRET_ACCESS_KEY:"
-read SECRET_ACCESS_KEY
+ACCESS_KEYS_PATH=$1
+if [ -z ${ACCESS_KEYS_PATH} ]
+then
+    echo -n "AWS_ACCESS_KEY_ID:"
+    read KEY_ID
+    echo -n "AWS_SECRET_ACCESS_KEY:"
+    read SECRET_ACCESS_KEY
 
+else
+    KEY_ID=$(awk -F, '{print $1}' ${ACCESS_KEYS_PATH} | tail -n 1)
+    SECRET_ACCESS_KEY=$(awk -F, '{print $2}' ${ACCESS_KEYS_PATH} | tail -n 1 | tr -d '\r')
+    echo $KEY_ID
+    echo $SECRET_ACCESS_KEY
+fi
 export AWS_ACCESS_KEY_ID=${KEY_ID}
+    echo $AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}
-export AWS_DEFAULT_REGION="eu-west-1"
+    echo $SECRET_ACCESS_KEY
 bash
