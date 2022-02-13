aea0596da27168884374f275cae16bda9c1005b5
orginux <orginux@protonmail.com>
2022-02-13 17:58:23 +0300

    fix bootstrap script

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,26 +7,25 @@ resource "aws_instance" "clickhouse_instace" {
   instance_type          = "t3.micro"
   key_name               = "aws-eu-north-1"
   vpc_security_group_ids = ["sg-0c369e68733470e29"]
-  user_data              = <<EOF
-    #! /bin/bash
-    DOCKER_COMPOSE_URL="https://raw.githubusercontent.com/orginux/clickhouse-test-env/main/docker-compose.yml"
-    DOCKER_COMPOSE_BIN_URL="https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)"
-    DOCKER_COMPOSE_BIN_PATH="/usr/local/bin/docker-compose"
 
-    sudo systemctl start docker
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(pathexpand("~/.ssh/aws/aws-eu-north-1.pem"))
+  }
 
-    if [ ! -f ${DOCKER_COMPOSE_BIN_PATH} ]
-    then
-        sudo curl -L ${DOCKER_COMPOSE_BIN_URL} -o ${DOCKER_COMPOSE_BIN_PATH}
-        sudo chmod +x ${DOCKER_COMPOSE_BIN_PATH}
-    fi
+  provisioner "file" {
+    source      = "install_clickhouse.sh"
+    destination = "/tmp/install_clickhouse.sh"
+  }
 
-    sudo usermod -aG docker $USER
-
-    wget -q ${DOCKER_COMPOSE_URL}
-
-    sudo docker-compose up -d
-  EOF
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install_clickhouse.sh",
+      "/tmp/install_clickhouse.sh",
+    ]
+  }
   tags = {
     Project = "ClickHouse"
     Env     = "test"
