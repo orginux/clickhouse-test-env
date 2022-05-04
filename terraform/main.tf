5050e2e125907076dbfbee625ce9855273d9ff98
Denis Gorobets <Denis.Gorobets@infobip.com>
2022-05-04 22:41:21 +0300

    rename key

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 resource "aws_instance" "clickhouse_instace" {
   ami                    = "ami-04989fe7f688013eb"
   instance_type          = "t3.micro"
-  key_name               = "aws-eu-north-1"
+  key_name               = "eu-north-1"
   vpc_security_group_ids = ["sg-0c369e68733470e29"]
 
   connection {
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file(pathexpand("~/.ssh/aws/aws-eu-north-1.pem"))
+    private_key = file(pathexpand("~/.ssh/aws/eu-north-1.pem"))
   }
 
   provisioner "file" {
